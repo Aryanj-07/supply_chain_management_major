4dd30d7c382b6e636fd56839ecef45ddbb046273
warkesiddharth <warkeofficial@gmail.com>
2023-08-21 16:13:42 +0530

    added consumer contract, with function of adding new consumer

---
+++ contracts/Consumer.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.9.0;
+
+import "./Users.sol";
+
+/**
+ * @title Consumer
+ * @dev Handles consumer functions.
+ */
+contract Consumer {
+    
+    using Users for Users.User;
+    Users.User private consumers;
+
+    function addConsumerr(address newConsumer) public {
+        require(!consumers.isExistingUser(newConsumer), "Consumer with this address already exists!");
+        consumers.addUser(newConsumer);
+    }
+
+}
