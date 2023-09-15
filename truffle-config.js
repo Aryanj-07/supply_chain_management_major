b8f42da087e4b112bf6c46145c07de21b7e0a8d6
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2023-09-15 13:11:20 +0530

    update migrations

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "appfrontend/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "appfrontend/contracts"),
   networks: {
     //to use custom networks, type: truffle console --network dev
     dev: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
